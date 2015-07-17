8c1d8da1135806aa2d90fb77bc6dbabb466e29a6
jnkFC <jithesh@fusioncharts.com>
2015-07-17 11:53:36 +0530

    dom3

--- LESSON/introduction_to_html/basic_DOM_element.js
+++ LESSON/introduction_to_html/basic_DOM_element.js
@@ -36,3 +36,58 @@ function json(){
 	JSON.stringify(subscribe);
 	console.log(subscribe);
 }
+function opt(){
+	if(document.getElementById("country").options[0].selected){
+		document.getElementById("country").options[0].disabled=true;
+		document.getElementById("country").options[1].disabled=false;
+		document.getElementById("state1").style.visibility="visible";
+		document.getElementById("state2").style.visibility="hidden";
+
+	}
+	else{
+		document.getElementById("country").options[0].disabled=false;
+		document.getElementById("country").options[1].disabled=true;
+		document.getElementById("state1").style.visibility="hidden";
+		document.getElementById("state2").style.visibility="visible";	
+	}
+	document.getElementById("state1_name").style.visibility="visible";
+	
+}
+function fav(){
+	if(document.getElementById("F_ball").checked){
+		document.getElementById("Reading_opt").style.display="none";
+		document.getElementById("Movie_opt").style.display="none";
+		document.getElementById("F_ball_opt").style.display="inline";
+	}
+	else  if(document.getElementById("Movie").checked){
+		document.getElementById("Reading_opt").style.display="none";
+		document.getElementById("F_ball_opt").style.display="none";
+		document.getElementById("Movie_opt").style.display="inline";
+	}
+	else{
+		document.getElementById("Movie_opt").style.display="none";
+		document.getElementById("F_ball_opt").style.display="none";
+		document.getElementById("Reading_opt").style.display="inline";
+	}
+	
+	
+}
+// assign function to onclick property of checkbox 
+//document.getElementById('active').onclick = function(){
+// call toggleSub when checkbox clicked 
+// toggleSub args: checkbox clicked on (this), id of element to show/hide 
+//toggleSub(this, 'active_sub');
+ //};
+  // called onclick of checkbox 
+ // function toggleSub(box, id){
+   // get reference to related content to display/hide 
+  // var el = document.getElementById(id);
+   //if ( box.checked ){
+    //el.style.display = 'block';
+//}
+//else 
+//{ el.style.display = 'none'; 
+//}
+//}
+
+
