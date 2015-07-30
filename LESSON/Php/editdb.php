273416a52aa6b006137a34c5ab0264bd83bc1bed
jnkFC <jithesh@fusioncharts.com>
2015-07-30 16:44:52 +0530

    done

--- LESSON/Php/editdb.php
+++ LESSON/Php/editdb.php
@@ -1,6 +1,9 @@
 <?
-include "db.inc.php";
-	$order = "UPDATE data_employees SET name='$name', address='$address' WHERE employees_number='$id'";
+	$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "subscriber";
+	$order = "UPDATE data_employees SET name='$name', email='$email', country='$country', phn_no='$phn_no', gender='$gender' WHERE employees_number='$id'";
 	          mysql_query($order);
-	header("location:edit.php");
+	header("location:editdb.php");
 	?>
